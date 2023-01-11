506c7e9a9448663a7fb8a9d49b08b4441b401650
Danila Rogozhnikov <94745416+m0libdEn@users.noreply.github.com>
2023-01-10 21:20:46 -0500

    Version 0.2
    Work with UNSPLASH API is connected!
    Now when you enter a specific city, a photo of the city itself is displayed in the background.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-const linkWeather = "https://api.openweathermap.org/data/2.5/weather?q="
-const linkWallpaper = "https://api.unsplash.com/"
 const city = document.getElementById('search')
 const btn = document.getElementById('btn')
 const log = document.getElementById('log')
+const wallpaper = document.getElementById('ibg')
+
 const getWeather = (city) =>{
+    const linkWeather = "https://api.openweathermap.org/data/2.5/weather?q="
     fetch(`${linkWeather}${city}&appid=2be90a0b516a3da1e524047c4f37dad6`)
     .then((res) => res.json())
     .then((data) => {
@@ -17,8 +18,17 @@ const getWallpaper = (city) =>{
  
 btn.addEventListener('click', ()=>{
     getWeather(city.value)
+    loadIbg(city.value)
 })
 
+function loadIbg(city){
+    const linkWallpaper = `https://api.unsplash.com/search/photos?page=1&query=${city}&client_id=Df0YGgE3Ak3PvYe_0MIreDZm3ps2MMqb77gl0QvpQLQ`
+    fetch(linkWallpaper)
+    .then((res) => res.json())
+    .then(data =>{
+        wallpaper.setAttribute('src', data.results[1].urls.raw)
+    })
+}
 
 function ibg(){
 	$.each($('.ibg'), function(index, val) {
@@ -27,4 +37,3 @@ function ibg(){
 		}
 	});
 }
-ibg();
