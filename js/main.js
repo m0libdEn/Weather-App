b544480aca00e3e814867e76cf50cc5262d7012a
Danila Rogozhnikov <94745416+m0libdEn@users.noreply.github.com>
2023-01-10 22:22:39 -0500

    Small update of previous vervsion
    Improving the system for searching for a photo for the background, as well as optimizing its loading by reducing the quality of the photo

--- js/main.js
+++ js/main.js
@@ -2,31 +2,31 @@ const city = document.getElementById('search')
 const btn = document.getElementById('btn')
 const log = document.getElementById('log')
 const wallpaper = document.getElementById('ibg')
+let countryOfLocation = ""
 
 const getWeather = (city) =>{
     const linkWeather = "https://api.openweathermap.org/data/2.5/weather?q="
     fetch(`${linkWeather}${city}&appid=2be90a0b516a3da1e524047c4f37dad6`)
     .then((res) => res.json())
     .then((data) => {
+        regionOfLocation = data.sys.country
+        const convertRegion = new Intl.DisplayNames(['en'], { type: 'region' });
+        countryOfLocation = convertRegion.of(regionOfLocation)
+        console.log(countryOfLocation);
         log.innerHTML = Math.round(data.main.temp - 273)
     })
 }
-
-const getWallpaper = (city) =>{
-    fetch(`${linkWallpaper}`)
-}
- 
 btn.addEventListener('click', ()=>{
     getWeather(city.value)
     loadIbg(city.value)
 })
 
 function loadIbg(city){
-    const linkWallpaper = `https://api.unsplash.com/search/photos?page=1&query=${city}&client_id=Df0YGgE3Ak3PvYe_0MIreDZm3ps2MMqb77gl0QvpQLQ`
+    const linkWallpaper = `https://api.unsplash.com/search/photos?page=1&orientation=landscape&query=${city} ${countryOfLocation}&client_id=Df0YGgE3Ak3PvYe_0MIreDZm3ps2MMqb77gl0QvpQLQ`
     fetch(linkWallpaper)
     .then((res) => res.json())
     .then(data =>{
-        wallpaper.setAttribute('src', data.results[1].urls.raw)
+        wallpaper.setAttribute('src', data.results[1].urls.regular)
     })
 }
 
